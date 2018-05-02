0931c462cb71238d38d8c27e3b2aa0c0440c05aa
nwactor <nickwactor@gmail.com>
2018-05-02 16:04:56 -0700

    send train information to clients every 60 seconds prototype

--- accessBartAPI.js
+++ accessBartAPI.js
@@ -6,14 +6,14 @@ const axios = require("axios");
 var stations = require("./Stations");
 
 const bartAPI = {
-	stations: stations,
-	getStationETDs: function() {
+	getStations: () => stations,
+	getTrainETDs: function(io) {
+		//get all stations with their trains from the BART api
 		axios.get("https://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&json=y&key=" + bartApiKey)
 		.then(apiResponse => {
 			for(let i = 0; i < stations.length; i++) {
-				setStationTrains(this.stations[i], apiResponse);
+				setStationTrains(stations[i], apiResponse, io);
 			}
-			console.log(this.stations);
 		}).catch(err => {
 			console.log(err);
 		});
@@ -22,12 +22,21 @@ const bartAPI = {
 
 //fill in the trains of this station with apiResponse
 //but, if apiResponse doesn't include this station, set its trains to empty array
-function setStationTrains(station, apiResponse) {
+function setStationTrains(station, apiResponse, io) {
 	var stationIsActive = false;
+	//take every station from the API response
+	//compare it to the station whose trains are being searched for (@param station)
+	//if they're the same station, set the trains
 	apiResponse.data.root.station.forEach(activeStation => {
 		if(activeStation.abbr === station.abbr) {
 			station.trains = activeStation.etd;
 			stationIsActive = true;
+
+			if(io != null) {
+				//emit to all clients in the station's "room"
+				io.to(station.abbr).emit("trainUpdate", station.trains);
+				console.log("sending updated trains");
+			}
 		}
 	});
 	//afterwards
--- server.js
+++ server.js
@@ -2,22 +2,63 @@ const express = require("express");
 const app = express();
 const server = require('http').createServer(app);  
 const socketio = require("socket.io");
-const websocket = socketio(server);
+const io = socketio(server);
 const bartAPI = require("./accessBartAPI");
 
 //set up routes for serving html for web app
 // var routes = require("./routes/routes");
 // app.use("/", routes);
 
-//socket handling
-// websocket.on('', (socket) => {
+//=======================================================================
+//=========================== socket handling ===========================
+//=======================================================================
 
-// });
+io.on('connection', client => {
+	console.log("Client connected: " + client);
+	client.station = null;
+	client.on('stationRequested', stationAbbr => onNewStationRequest(stationAbbr, client));
+});
+
+//make the client a reciever of the new station, instead of their old one
+//call helper function send client the trains for given station
+function onNewStationRequest(stationAbbr, client) {
+	client.leave(client.station);
+	client.station = stationAbbr;
+	client.join(client.station);
+	sendUpdatedTrains(client, stationAbbr);
+}
+
+//send a client the trains for given station 
+function sendUpdatedTrains(client, stationAbbr) {
+	bartAPI.getStations().forEach(station => {
+		if(station.abbr === stationAbbr) {
+			client.emit("trainUpdate", station.trains);
+			return;
+		}
+	});
+}
+
+//=======================================================================
+//========================= end socket handling =========================
+//=======================================================================
+
+function mainUpdateLoop() {
+	console.log("entering update loop");
+	bartAPI.getTrainETDs(sendUpdatesToClients);
+}
+
+function sendUpdatesToClients() {
+	console.log("Sent updates to clients!");
+}
+
+//check for new trains every 60 seconds
+// bartAPI.getTrainETDs(io);
+// var checkBartAPI = setInterval(bartAPI.getTrainETDs, 60000, io);
+
 
 //start app listening
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
 	console.log("App listening on PORT " + PORT);
 });
-
-bartAPI.getStationETDs();
